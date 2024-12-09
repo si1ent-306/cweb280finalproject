0602b53e2e26abd8179dfeb1094d2016814d39a7
Elliott Wagner <elliottjwag@hotmail.com>
2024-12-08 19:20:54 -0600

    m jkflogxojkm

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,18 +1,39 @@
 <script setup>
 
 </script>
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        email: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log('Form Submitted with Data:', this.formData);
+      // You can reset the form if needed:
+      this.formData.email = '';
+      this.formData.password = '';
+    },
+  },
+};
+</script>
 
 <template>
 <h1>Login</h1>
   <div id="login-container">
-  <form id="login-form">
+  <form id="login-form" @submit.prevent="handleSubmit">
     <label for="email">Email</label>
-    <input type="email" id="email" name="email" required>
+    <input type="email" id="email" name="email" v-model="formData.email" required>
     <label for="password">Password</label>
-    <input type="password" id="password" name="password" required>
+    <input type="password" id="password" name="password" v-model="formData.password" required>
     <button type="submit">Login</button>
   </form>
   </div>
+
 </template>
 
 <style >
@@ -30,3 +51,4 @@
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 </style>
+
